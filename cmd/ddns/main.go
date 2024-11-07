b3051f210625432bbdc2c7537bb3a72bc0d202cf
Mmx <green.dusk8264@fastmail.com>
2024-11-07 13:08:59 +0800

    fix: logs in main logic

--- cmd/ddns/main.go
+++ cmd/ddns/main.go
@@ -5,33 +5,38 @@ import (
 	"github.com/Mmx233/ddns/internal/config"
 	"github.com/Mmx233/ddns/stun"
 	log "github.com/sirupsen/logrus"
+	"net"
 )
 
-func RunDDNS(ctx context.Context, network, dnsType string) error {
+func RunDDNS(ctx context.Context, network, dnsType string) (net.IP, error) {
 	ctx, cancel := context.WithTimeout(ctx, config.Timeout)
 	defer cancel()
 
 	ip, err := stun.Dial(ctx, network, config.Env.STUN)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	return config.DNS.SetDomainRecord(config.Env.Domain, dnsType, ip.String())
+	return ip, config.DNS.SetDomainRecord(config.Env.Domain, dnsType, ip.String())
 }
 
 func main() {
 	ctx := context.TODO()
 
 	if config.Env.Ipv4 {
-		err := RunDDNS(ctx, "udp4", "A")
+		ip, err := RunDDNS(ctx, "udp4", "A")
 		if err != nil {
-			log.Fatalln("run ddns for ipv4 failed:", err)
+			log.Errorln("run ddns for ipv4 failed:", err)
+		} else {
+			log.Infof("successfully set %s A record to %s", ip.String(), config.Env.Domain)
 		}
 	}
 	if config.Env.Ipv6 {
-		err := RunDDNS(ctx, "udp6", "AAAA")
+		ip, err := RunDDNS(ctx, "udp6", "AAAA")
 		if err != nil {
-			log.Fatalln("run ddns for ipv6 failed:", err)
+			log.Errorln("run ddns for ipv6 failed:", err)
+		} else {
+			log.Infof("successfully set %s AAAA record to %s", ip.String(), config.Env.Domain)
 		}
 	}
 }
