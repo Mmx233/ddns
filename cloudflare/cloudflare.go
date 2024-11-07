2ce0ca00ef197b68be7f96a34a49a278d176cf4e
Mmx <green.dusk8264@fastmail.com>
2024-11-07 11:11:36 +0800

    feat: cloudflare function

---
+++ cloudflare/cloudflare.go
@@ -0,0 +1,67 @@
+package cloudflare
+
+import (
+	"context"
+	"errors"
+	"github.com/cloudflare/cloudflare-go"
+	"net/http"
+)
+
+type Cloudflare struct {
+	Zone  string `json:"zone" yaml:"zone"`
+	Token string `json:"token" yaml:"token"`
+}
+
+type DnsProvider struct {
+	Api          *cloudflare.API
+	TTL          int
+	ZoneResource *cloudflare.ResourceContainer
+	Cloudflare
+}
+
+func New(ttl int, conf Cloudflare, Http *http.Client) (*DnsProvider, error) {
+	var p = DnsProvider{
+		TTL:        ttl,
+		Cloudflare: conf,
+	}
+	if p.Zone == "" {
+		return nil, errors.New("cloudflare zone can't be empty")
+	}
+	if p.Token == "" {
+		return nil, errors.New("cloudflare token can't be empty")
+	}
+	p.ZoneResource = cloudflare.ZoneIdentifier(p.Zone)
+	var err error
+	p.Api, err = cloudflare.NewWithAPIToken(p.Token, cloudflare.HTTPClient(Http))
+	return &p, err
+}
+
+func (a DnsProvider) SetDomainRecord(domain, Type, ip string) error {
+	records, _, err := a.Api.ListDNSRecords(context.Background(), a.ZoneResource, cloudflare.ListDNSRecordsParams{
+		Type: Type,
+		Name: domain,
+	})
+	if err != nil {
+		return err
+	}
+
+	if len(records) == 0 {
+		_, err = a.Api.CreateDNSRecord(context.Background(), a.ZoneResource, cloudflare.CreateDNSRecordParams{
+			Type:    Type,
+			Name:    domain,
+			Content: ip,
+			TTL:     a.TTL,
+		})
+		return err
+	} else {
+		record := records[0]
+		if record.Content == ip {
+			return nil
+		}
+		_, err = a.Api.UpdateDNSRecord(context.Background(), a.ZoneResource, cloudflare.UpdateDNSRecordParams{
+			ID:      record.ID,
+			Content: ip,
+		})
+		return err
+	}
+}
--- go.mod
+++ go.mod
@@ -2,12 +2,21 @@ module github.com/Mmx233/ddns
 
 go 1.23.2
 
-require github.com/pion/stun v0.6.1
+require (
+	github.com/cloudflare/cloudflare-go v0.109.0
+	github.com/pion/stun v0.6.1
+)
 
 require (
+	github.com/goccy/go-json v0.10.3 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/pion/dtls/v2 v2.2.7 // indirect
 	github.com/pion/logging v0.2.2 // indirect
 	github.com/pion/transport/v2 v2.2.1 // indirect
-	golang.org/x/crypto v0.8.0 // indirect
-	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/crypto v0.28.0 // indirect
+	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	golang.org/x/time v0.7.0 // indirect
 )
